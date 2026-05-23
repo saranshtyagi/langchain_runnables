917d67ddd7115e22d37f5fbe89ab5942ef7d9475
saranshtyagi <18saranshtyagi2004@gmail.com>
2026-05-23 10:57:45 +0530

    Add runnable implementation for joke generation and explanation

---
+++ runnable_passthrough.py
@@ -0,0 +1,41 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.2-1B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.7
+)
+
+model = ChatHuggingFace(llm = llm)
+
+prompt1 = PromptTemplate(
+    template="Write a joke about {topic}", 
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="Explain the joke - {joke}", 
+    input_variables=["joke"]
+)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    "joke": RunnablePassthrough(), 
+    "explanation": RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({"topic": "programming"})
+print(result)
