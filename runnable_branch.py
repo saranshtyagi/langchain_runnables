652efb95af48e192d134bfca94cf9a3da94ac4d4
saranshtyagi <18saranshtyagi2004@gmail.com>
2026-05-23 12:31:14 +0530

    Add runnable implementation for report generation with conditional summarization

---
+++ runnable_branch.py
@@ -0,0 +1,41 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda, RunnableBranch
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.2-1B-Instruct",
+    task="text-generation",
+    temperature=0.7
+)
+
+model = ChatHuggingFace(llm = llm)
+
+parser = StrOutputParser()
+
+prompt1 = PromptTemplate(
+    template="Write a detailed report on the {topic}", 
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="Summarize the following text \n {text}", 
+    input_variables=["text"]
+)
+
+report_gen_chain = RunnableSequence(prompt1, model, parser)
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split()) > 200, RunnableSequence(prompt2, model, parser)), 
+    RunnablePassthrough() #default behavior
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+result = final_chain.invoke({"topic": "Iran vs US-Israel Conflict"})
+
+print(result)
